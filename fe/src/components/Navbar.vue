2ab0af08517444632c6790c59bd3b409a284dd21
umutural02 <umutural1596@gmail.com>
2025-05-09 14:06:47 +0300

    Create navbar component and home page

---
+++ fe/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <div class="px-4 sm:px-8">
+      <div class="flex justify-between h-16">
+        <div class="flex items-center">
+          <router-link to="/" class="flex items-center">
+            <img src="../assets/logo.png" alt="Logo" class="h-8 w-auto" />
+          </router-link>
+        </div>
+        <div class="flex items-center space-x-4">
+          <router-link to="/login" class="text-white">Sign in</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ fe/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="h-full flex items-center justify-center">
+    <h1 class="text-white">Home</h1>
+  </main>
+</template>
