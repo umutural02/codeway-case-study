90325001217e49b5537c0975ae872a5a498226c9
umutural02 <umutural1596@gmail.com>
2025-05-10 09:44:17 +0300

    implement dynamic navbar and protected routes

--- fe/src/components/Navbar.vue
+++ fe/src/components/Navbar.vue
@@ -8,9 +8,47 @@
           </router-link>
         </div>
         <div class="flex items-center space-x-4">
-          <router-link to="/login" class="text-white">Sign in</router-link>
+          <template v-if="user">
+            <div class="flex items-center space-x-2">
+              <button
+                @click="handleSignOut"
+                class="text-white hover:text-gray-300 transition-colors"
+              >
+                Sign out
+              </button>
+            </div>
+          </template>
+          <template v-else>
+            <router-link to="/login" class="text-white">Sign in</router-link>
+          </template>
         </div>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const user = ref(null);
+const router = useRouter();
+const auth = getAuth();
+
+onMounted(() => {
+  onAuthStateChanged(auth, (currentUser) => {
+    user.value = currentUser;
+    console.log(user.value);
+  });
+});
+
+const handleSignOut = async () => {
+  try {
+    await signOut(auth);
+    router.push("/login");
+  } catch (error) {
+    console.error("Error signing out:", error);
+  }
+};
+</script>
--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,13 +10,47 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/login",
       name: "login",
       component: Login,
+      meta: {
+        requiresAuth: false,
+      },
     },
   ],
+});
+
+/* Get the current user asynchronously */
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
+
+/* Check if the user is authenticated before accessing a route */
+router.beforeEach(async (to, from, next) => {
+  const currentUser = await getCurrentUser();
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (currentUser) {
+      next();
+    } else {
+      next({ name: "login" });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
