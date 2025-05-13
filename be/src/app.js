6b4591586db460f9a0bed9179aef328f1ab57fe0
umutural02 <umutural1596@gmail.com>
2025-05-13 21:45:49 +0300

    add cors to backend

--- be/src/app.js
+++ be/src/app.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import parameterRoutes from "./routes/parameters.js";
 import { checkApiToken } from "./middlewares/checkApiToken.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(checkApiToken);
