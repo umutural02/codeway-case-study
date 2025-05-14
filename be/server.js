8dbe065a0d98f9f2fd610acf533cb74cc6439434
umutural02 <umutural1596@gmail.com>
2025-05-14 22:42:06 +0300

    Console message updated

--- be/server.js
+++ be/server.js
@@ -3,5 +3,5 @@ import app from "./src/app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
